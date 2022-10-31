003b94795917d46b68a4aa8e4e548714056448a7
berekt24 <bashenafi35@gmail.com>
2022-10-31 18:32:01 -0400

    first question

---
+++ script.js
@@ -0,0 +1,7 @@
+var questions = [
+  {
+    question: "Which element is usually used as the title of the page and/or first header?",
+    options: ["<p>", "<div>", "<h1>", "footer"],
+    answer: 3
+  }  
+]
