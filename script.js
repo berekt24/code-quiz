7880f4ea4ccd8ea190978d7481b7f419d09d6cdc
berekt24 <bashenafi35@gmail.com>
2022-10-31 19:07:14 -0400

    added questions to array

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
 var questions = [
   {
     question: "Which element is usually used as the title of the page and/or first header?",
-    options: ["<p>", "<div>", "<h1>", "footer"],
+    options: ["<p>", "<div>", "<h1>", "<ryan>"],
     answer: 3
-  }  
+  },
+  
+  {
+    question: "Which option is a class selector?",
+    options: ["#box", "section", ".header", "class = henry"],
+    answer: 3
+  },
+
+  {
+    question: "Which datatype has a true or false value?",
+    options: ["string", "number", "boolean", "willis"],
+    answer: 3
+  },
+
+  {
+    question: "Which method adds an element to the end of an array?",
+    options: ["splice", "sort", "push", "vrabel"],
+    answer: 3
+  },
+
+  {
+    question: "What does the method getElementById do?",
+    options: ["It gets the id from the Titans.", "It gets the id from the browser", "It gets the id from the JavaScript.", "It gets the id from the HTML."],
+    answer: 4
+  }
 ]
+console.log(questions)
