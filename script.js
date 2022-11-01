f1f0fb3f24b6630275624ad7438b7376f08fe3d7
berekt24 <bashenafi35@gmail.com>
2022-10-31 20:37:25 -0400

    added function skeleton

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var startQuiz = document.querySelector(".start");
+var currentQuestionIndex = 0;
 
 var questions = [
   {
@@ -32,8 +33,40 @@ var questions = [
   }
 ]
 
-startQuiz.addEventListener("click", function () {
+function startQuiz() {
+    // hides the start screen
+   
+    // un-hides questions section
+  
+    // starts timer
+    // show starting time
     
+    // calls another function maybe called:
+    // getQuestion();
+  }
+
+  function getQuestion() {
+    // get current question object from array
+    var currentQuestion = questions[currentQuestionIndex];
+  
+    // update title in the html with current question
+  
+  
+    // clear out any old question choices
+   
+  
+    // loop over choices and create new button for each choice
+     
+  
+     
+  
+      // display on the page by appending
+      
+    }
+  
+
+startQuiz.addEventListener("click", function () {
+
 })
 
 console.log(questions)
