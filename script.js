4d40752477ade0d67ca36aefc527c8ee1f1d0ad9
berekt24 <bashenafi35@gmail.com>
2022-11-01 21:25:22 -0400

    fix for time function and questions

--- script.js
+++ script.js
@@ -2,36 +2,37 @@ var startQuizEl = document.querySelector("#start");
 var currentQuestionIndex = 0;
 var timeEl = document.querySelector(".time");
 var timerElement = document.querySelector(".timer-count");
-var timerCount;
+var timerCount = 60;
+var questionTitle = document.querySelector("questionTitle")
 var questions = [
   {
     question: "Which element is usually used as the title of the page and/or first header?",
-    options: ["<p>", "<div>", "<h1>", "<ryan>"],
-    answer: 3
+    options: ["a. <p>", "b. <div>", "c. <h1>", "d. <ryan>"],
+    answer: "c. <h1>"
   },
   
   {
     question: "Which option is a class selector?",
-    options: ["#box", "section", ".header", "class = henry"],
-    answer: 3
+    options: ["a. #box", "b. section", "c. .header", "d. class = henry"],
+    answer: "c. .header"
   },
 
   {
     question: "Which datatype has a true or false value?",
-    options: ["string", "number", "boolean", "willis"],
-    answer: 3
+    options: ["a. string", "b. number", "c. boolean", "d. willis"],
+    answer: "c. boolean"
   },
 
   {
     question: "Which method adds an element to the end of an array?",
-    options: ["splice", "sort", "push", "vrabel"],
-    answer: 3
+    options: ["a. splice", "b. sort", "c. push", "d. vrabel"],
+    answer: "c. push"
   },
 
   {
     question: "What does the method getElementById do?",
-    options: ["It gets the id from the Titans.", "It gets the id from the browser", "It gets the id from the JavaScript.", "It gets the id from the HTML."],
-    answer: 4
+    options: ["a. It gets the id from the Titans.", "b. It gets the id from the browser", "c. It gets the id from the JavaScript.", "d. It gets the id from the HTML."],
+    answer: "d. It gets the id from the HTML."
   }
 ]
 
@@ -56,7 +57,7 @@ function startQuiz() {
         // Sets timer
         timer = setInterval(function() {
           timerCount--;
-          timerElement.textContent = timerCount;
+          timeEl.textContent = timerCount;
           
           
           }
@@ -64,30 +65,24 @@ function startQuiz() {
       }
         
 
-
-
-
-
-
-  
-
   function getQuestion() {
     // get current question object from array
     var currentQuestion = questions[currentQuestionIndex];
   
     // update title in the html with current question
-    
+    questionTitle.textContent = questions[currentQuestionIndex].question;
+    optionA.textContent = questions[currentQuestionIndex].options[0];
+    optionB.textContent = questions[currentQuestionIndex].options[1];
+    optionC.textContent = questions[currentQuestionIndex].options[2];
+    optionD.textContent = questions[currentQuestionIndex].options[3];
   
     // clear out any old question choices
    
-  
-    // loop over choices and create new button for each choice
-     
-  
-     
-  
-      // display on the page by appending
       
+    }
+
+    function checkAnswers() {
+
     }
   
     startQuizEl.addEventListener("click", startQuiz);
